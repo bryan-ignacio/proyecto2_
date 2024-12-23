30430c79f27d6f7f2f4f77be5bdb21b5712d884c
Bryan Ignacio <bryan-ignacio@outlook.com>
2024-12-23 17:47:39 -0600

    clase Viaje added

--- Ruta.py
+++ Ruta.py
@@ -1,2 +1,26 @@
 class Ruta:
-    pass
+    def __init__(self, origen, destino, tiempo):
+        self.__origen = origen
+        self.__destino = destino
+        self.__tiempo = tiempo
+
+    def get_origen(self):
+        return self.__origen
+
+    def set_origen(self, origen):
+        self.__origen = origen
+
+    def get_destino(self):
+        return self.__destino
+
+    def set_destino(self, destino):
+        self.__destino = destino
+
+    def get_tiempo(self):
+        return self.__tiempo
+
+    def set_tiempo(self, tiempo):
+        self.__tiempo = tiempo
+
+    def __str__(self):
+        return f'Ruta->[origen: {self.__origen}, destino: {self.__destino}, tiempo:{self.__tiempo}]'
--- Viaje.py
+++ Viaje.py
@@ -1,2 +1,43 @@
 class Viaje:
-    pass
+    def __init__(self, origen, destino, fecha, vehiculo, camino):
+        self.__origen = origen
+        self.__destino = destino
+        self.__fecha = fecha
+        self.__vehiculo = vehiculo
+        self.__camino = camino
+
+    def get_origen(self):
+        return self.__origen
+
+    def set_origen(self, origen):
+        self.__origen = origen
+
+    def get_destino(self):
+        return self.__destino
+
+    def set_destino(self, destino):
+        self.__destino = destino
+
+    def get_fecha(self):
+        return self.__fecha
+
+    def set_fecha(self, fecha):
+        self.__fecha = fecha
+
+    def get_vehiculo(self):
+        return self.__vehiculo
+
+    def set_vehiculo(self, vehiculo):
+        self.__vehiculo = vehiculo
+
+    def get_camino(self):
+        return self.__camino
+
+    def set_camino(self, camino):
+        self.__camino = camino
+
+    def __str__(self):
+        return (f'Viaje->[origen: {self.__origen}, destino: {self.__destino}, \n'
+                f'fecha: {self.__fecha}, vehiculo: {self.__vehiculo}, camino: {self.__camino}]')
+
+    
