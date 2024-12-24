c5874cfac2de8fe48ba5160702840c863c8fd9da
Bryan Ignacio <bryan-ignacio@outlook.com>
2024-12-23 19:13:58 -0600

    metodo instertar primero agregado

--- ListaClientes.py
+++ ListaClientes.py
@@ -1,3 +1,7 @@
+from Cliente import Cliente
+from NodeCliente import NodeCliente
+
+
 class ListaClientes:
     def __init__(self):
         self.__primero = None
@@ -16,13 +20,45 @@ class ListaClientes:
         self.__ultimo = ultimo
 
     def esta_vacia(self) -> bool:
-        pass
+        return self.__primero is None and self.__ultimo is None
 
-    def insertar_inicio(self):
-        pass
+    def insertar_inicio(self, cliente):  # Crear el nuevo nodo cliente
+        new_node_cliente = NodeCliente(
+            cliente.get_dpi(),
+            cliente.get_nombres(),
+            cliente.get_apellidos(),
+            cliente.get_genero(),
+            cliente.get_telefono(),
+            cliente.get_direccion()
+        )
+        if self.esta_vacia():
+            # Primera inserción, el nodo apunta a sí mismo
+            new_node_cliente.set_siguiente(new_node_cliente)
+            new_node_cliente.set_anterior(new_node_cliente)
+            self.__primero = self.__ultimo = new_node_cliente
+        else:
+            # Conectamos el nuevo nodo al inicio y al final
+            new_node_cliente.set_siguiente(self.__primero)
+            new_node_cliente.set_anterior(self.__ultimo)
+            self.__primero.set_anterior(new_node_cliente)
+            self.__ultimo.set_siguiente(new_node_cliente)
+            self.__primero = new_node_cliente
 
     def imprimir(self):
-        pass
+        if self.esta_vacia():
+            print("La lista está vacía.")
+            return
+        actual = self.__primero
+        elementos = []
+        while True:
+            cliente = actual.get_cliente()
+            elementos.append(f'[{cliente.get_dpi()} - {cliente.get_nombres()}]')
+            actual = actual.get_siguiente()
+            if actual == self.__primero:  # Se ha completado un ciclo
+                break
+
+        print(" --> ".join(elementos))
+
 
     def buscar_node_cliente(self):
         pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Cliente import Cliente
+from ListaClientes import ListaClientes
 from Vehiculo import Vehiculo
 
 if __name__ == '__main__':
@@ -9,6 +10,38 @@ if __name__ == '__main__':
                  "30817780",
                  "52 calle 16-00 zona 12")
 
-    v1 = Vehiculo("BYJ356", "Toyata", "Tercel", "30000")
-    print(v1)
-    print(c1)
+    c2 = Cliente("3748291048602",
+                 "María",
+                 "López",
+                 "F",
+                 "32017654",
+                 "Avenida Las Rosas 12-45 zona 5")
+
+    c3 = Cliente("3905617392403",
+                 "Carlos",
+                 "Hernández",
+                 "M",
+                 "31098765",
+                 "Boulevard Los Próceres 8-22 zona 10")
+
+    c4 = Cliente("3059284736104",
+                 "Ana",
+                 "Martínez",
+                 "F",
+                 "31987654",
+                 "Colonia El Prado 3-14 zona 7")
+
+    c5 = Cliente("3284015629305",
+                 "Jorge",
+                 "Gómez",
+                 "M",
+                 "32234567",
+                 "Residenciales Las Flores 2-18 zona 11")
+
+    lista_clientes = ListaClientes()
+    lista_clientes.insertar_inicio(c1)
+    lista_clientes.insertar_inicio(c2)
+    lista_clientes.insertar_inicio(c3)
+    lista_clientes.insertar_inicio(c4)
+    lista_clientes.insertar_inicio(c5)
+    lista_clientes.imprimir()
